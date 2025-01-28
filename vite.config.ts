8a058796ac525fb9c478616052fd98288996fcb9
christiancrisologo <christian.crisologo@learnosity.com>
2025-01-29 10:48:22 +1100

    revert to base path to fix the gh page

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
+// filepath: /Users/christiancrisologo/dev/projs/chrome-timer-ext/vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/timer-chrome-ext/',
   build: {
     rollupOptions: {
       input: {
